54371dd2e971486a29124d04ae823259d8e77739
Kobe0816 <kobes0816@yahoo.com>
2026-02-12 15:40:57 -0600

    Sprint0 Complete

--- src/pegSolitare/gui.java
+++ src/pegSolitare/gui.java
@@ -1,40 +1,57 @@
 package pegSolitare;
 
+import java.awt.Frame;
+
 import javax.swing.*;
 
 // Main class
 public class gui {
+// Creates GUI
+	public static JFrame createGUI() {
+		 // Creating JFrame
+	     JFrame frame = new JFrame();
 
- // Main driver method
+	     // Creating GUI Elements and Setting Bounds
+	     JButton button = new JButton("New Game");
+	     button.setBounds(0, 0, 100, 30);
+	     JCheckBox recordBox = new JCheckBox("Record Game");
+	     recordBox.setBounds(0, 160, 150, 25);
+	     JRadioButton board1 = new JRadioButton("English");
+	     JRadioButton board2 = new JRadioButton("European");
+	     JRadioButton board3 = new JRadioButton("Diamond");
+	     JRadioButton board4 = new JRadioButton("Triangle");
+	     JLabel boardTypes = new JLabel("Board Type");
+	     
+	     // Boards JPanel and Their Elements
+	     JPanel boards = new JPanel();
+	     boards.add(boardTypes);
+	     boards.add(board1);
+	     boards.add(board2);
+	     boards.add(board3);
+	     boards.add(board4);
+	     boards.setBounds(0, 50, 100, 120);
+	     boards.setLayout(new BoxLayout(boards, BoxLayout.Y_AXIS));
+
+	     // Board Button Group
+	     ButtonGroup boardGroup = new ButtonGroup();
+	     boardGroup.add(board1);
+	     boardGroup.add(board2);
+	     boardGroup.add(board3);
+	     boardGroup.add(board4);
+	     
+	     // JFrame Elements
+	     frame.add(button);
+	     frame.add(recordBox);
+	     frame.add(boards);
+	     
+	     // JFrame Properties
+	     frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+	     frame.setLayout(null);
+	     frame.setVisible(true);
+	return frame;
+	}
  public static void main(String[] args)
  {
-     // Creating instance of JFrame
-     JFrame frame = new JFrame();
-
-     // Creating instances of JElements
-     JButton button = new JButton("Button");
-     JCheckBox checkBox = new JCheckBox("Checkbox");
-     JRadioButton radio1 = new JRadioButton("Radio1");
-     JRadioButton radio2 = new JRadioButton("Radio2");
-
-     // Make a JPanel and add all JElements to it
-     JPanel panel = new JPanel();
-     panel.add(button);
-     panel.add(checkBox);
-     panel.add(radio1);
-     panel.add(radio2);
-     
-     panel.setBounds(150, 200, 220, 100);
-     // Button Group
-     ButtonGroup radioGroup = new ButtonGroup();
-     radioGroup.add(radio1);
-     radioGroup.add(radio2);
-     
-     // JFrame JElements
-     frame.add(panel);
-     // JFrame properties
-     frame.setSize(500, 600);
-     frame.setLayout(null);
-     frame.setVisible(true);
+    createGUI();
  }
 }
